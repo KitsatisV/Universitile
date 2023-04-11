7a0b361d5ac2212fdf473b591f9c47fcf46e9a3b
KitsatisV <vkitsatis@gmail.com>
2023-04-12 01:08:17 +0300

    MudDialog on RadzenCalendar, MudCard and MudBadge on RadzenList
    Added a MudDialog razor component for the calendar and now it opens onclick for each event, added MudCards in the RadzenList and MudBadges on each card, some refinement and tweaking needed

--- Universitile01/Data/Announcement.cs
+++ Universitile01/Data/Announcement.cs
@@ -2,11 +2,13 @@
 {
     public class Announcement
     {
-        private string _title;
-        private string _description;
-        private string _date;
-        private string _school;
-        private string _direction;
+        private string  _title;
+        private string  _description;
+        private string  _date;
+        private string  _school;
+        private string  _direction;
+        private bool    _importance;
+        private bool    _isNotRead;
 
         public string Title
         {
@@ -33,14 +35,26 @@
             get { return _direction; }
             set { _direction = value; }
         }
+        public bool Importance
+        {
+            get { return _importance; }
+            set { _importance = value; }
+        }
+        public bool IsNotRead
+        {
+            get { return _isNotRead; }
+            set { _isNotRead = value; }
+        }
 
-        public Announcement(string title, string description, string date, string school, string direction)
+        public Announcement(string title, string description, string date, string school, string direction, bool importance, bool isNotRead)
         {
             this._title = title;
             this._description = description;
             this._date = date;
             this._school = school;
             this._direction = direction;
+            this._importance = importance;
+            this._isNotRead = isNotRead;
         }
     }
 }
