a902615dbf86408e8012d802b992df0d04ed5ad8
KitsatisV <vkitsatis@gmail.com>
2023-04-19 01:38:31 +0300

    Login, Azure Database, Identity, PersonalInfo
    Implemented the Identity service on the Identity folder razor pages, connected the azure database to the project, Identity still can't create its tables in the database (needs to be resolved ASAP), added the Session class for the PersonalInfo page

---
+++ Universitile01/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Universitile01.Data
+{
+	public class DataContext : IdentityDbContext
+	{
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
