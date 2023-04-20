3d8382bcd8a3de032c47c0b4de116c9f69185c68
KitsatisV <vkitsatis@gmail.com>
2023-04-20 18:34:14 +0300

    Login asp-validation error messages
    Now all the error messages for the asp-validation are properly displayed including empty fields, regex email check and incorrect email and/or password

--- Universitile01/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Universitile01/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using System.ComponentModel.DataAnnotations;
 
 namespace Universitile01.Areas.Identity.Pages.Account
@@ -29,6 +30,8 @@ namespace Universitile01.Areas.Identity.Pages.Account
 				{
 					return LocalRedirect(ReturnUrl);
 				}
+
+				ModelState.AddModelError("", "Email and/or Password incorrect!");
 			}
 
 			return Page();
