22c04a07ace5988749b21eabf4cbd407193d767f
KitsatisV <vkitsatis@gmail.com>
2023-04-21 02:41:45 +0300

    Undid the comment in Program

--- Universitile01/Program.cs
+++ Universitile01/Program.cs
@@ -37,13 +37,13 @@ builder.Services.AddMudServices();
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//	app.UseExceptionHandler("/Error");
-//	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//	app.UseHsts();
-//}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
