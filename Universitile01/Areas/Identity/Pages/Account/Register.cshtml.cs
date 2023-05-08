695fd2bdb25451064463b12cb63513a80d0bfb94
GeorgeVlachakisUni <g.vlachakis@mc-class.gr>
2023-05-08 19:24:51 +0300

    initialized admin page for applying course to students.
    Co-Authored-By: Ieremias Giantzaklidis <makleikle@users.noreply.github.com>

--- Universitile01/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Universitile01/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,26 +14,18 @@ namespace Universitile01.Areas.Identity.Pages.Account
 	{
 		private readonly SignInManager<IdentityUser> _signInManager;
 		private readonly UserManager<IdentityUser> _userManager;
-		private readonly RoleManager<IdentityRole> _roleManager;
-		private List<PersonalInfo> _personalInfo = new List<PersonalInfo>(); 
 		private string _connectionString = "Server=universtile.mysql.database.azure.com;User ID=azureuser;Password=7TI2K6O0O1ZL6SIUE6BDMGLDK*;Database=universitiledatabase;SslMode=Required;SslCa=DigiCertGlobalRootCA.crt.pem;TlsVersion=TLS 1.2";
 
 		public RegisterModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
 		{
 			_signInManager = signInManager;
 			_userManager = userManager;
-			_roleManager = roleManager;
 		}
 
 		[BindProperty]
 		public InputModel Input { get; set; }
 
 		public string ReturnUrl { get; set; }
-
-		//public void OnGet()
-		//{
-		//	ReturnUrl = Url.Content("~/");
-		//}
 
 		public async Task<IActionResult> OnPostAsync()
 		{
@@ -43,13 +35,9 @@ namespace Universitile01.Areas.Identity.Pages.Account
 			{
 				var identity = new IdentityUser { UserName = Input.Email, Email = Input.Email };
 				var result = await _userManager.CreateAsync(identity, Input.Password);
-
 				var role = new IdentityRole(Input.Role);
-				var addRoleResult = await _roleManager.CreateAsync(role);
 
-				var addUserRoleResult = await _userManager.AddToRoleAsync(identity, Input.Role);
-
-				if (true/*result.Succeeded && addRoleResult.Succeeded && addUserRoleResult.Succeeded*/)
+				if (result.Succeeded)
 				{
 					await _signInManager.SignInAsync(identity, isPersistent: false);
 
