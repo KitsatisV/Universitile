db7b0da925d525bc96be51d8c6b259c2ffe7ca46
Ieremias Giantzaklidis <makleigiant@gmail.com>
2023-05-10 03:47:40 +0300

    cleanup after merge
    removed unnecessary code
    
    Co-Authored-By: GeorgeVlachakisUni <120213955+GeorgeVlachakisUni@users.noreply.github.com>

--- Universitile01/Data/Grades.cs
+++ Universitile01/Data/Grades.cs
@@ -2,28 +2,28 @@
 {
     public class Grades
     {
-		private string _module;
-		private int _grade;
+        private string _module;
+        private int _grade;
 
-		public string Module
-		{
-			get { return _module; }
-			set { _module = value; }
-		}
-		public int Grade
-		{
-			get { return _grade; }
-			set { _grade = value; }
-		}
+        public string Module
+        {
+            get { return _module; }
+            set { _module = value; }
+        }
+        public int Grade
+        {
+            get { return _grade; }
+            set { _grade = value; }
+        }
 
         public Grades()
         {
-            
+
         }
         public Grades(string module, int grade)
         {
             _module = module;
-			_grade = grade;
+            _grade = grade;
         }
     }
 }
